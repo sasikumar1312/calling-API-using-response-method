a386c913fdec42f35ddb54c5f342e7b721149b61
sasikumar1312 <89637727+sasikumar1312@users.noreply.github.com>
2021-08-27 03:08:32 -0700

    Add files via upload

---
+++ call api/prj2callapi/prj2callapi/Controllers/APIController.cs
@@ -0,0 +1,23 @@
+﻿using MaheApi.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prj2callapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        public List<string> StateIndex()
+        {
+            var responseString = ApiCall.GetApi("https://localhost:44335/Web");
+            var rootobject = new JavaScriptSerializer().Deserialize<List<string>>(responseString);
+            return rootobject;
+        }
+    }
+}
